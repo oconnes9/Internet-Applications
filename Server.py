51adaabf1b9f542fd06046032528138aa43d7196
oconnes9 <oconnes9@tcd.ie>
2017-10-16 15:19:17 +0100

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,25 @@
+# Sean O'Connell 14315362
+# TCP Server
+#
+import struct
+import binascii
+from socket import *
+
+serverPort = 12000
+IPaddress = '123.123.12'
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print 'The server is ready to receive'
+connectionSocket, addr = serverSocket.accept()
+
+
+while 1:
+    
+    recieved = connectionSocket.recv(2048)
+    if recieved.strip() == "KILL_SERVICE\n":
+        connectionSocket.close()
+        sys.exit("Received disconnect message.  Shutting down.")
+        connectionSocket.send("nak")
+    elif recieved.strip() == "HELO text":
+        print 'HELO text\nIP: ', IPaddress, '\n', 'Port: ', serverPort, '\n'

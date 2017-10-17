1328e8aa6d22a9cbf0c0addec9dfda5960917f16
oconnes9 <oconnes9@tcd.ie>
2017-10-17 17:43:01 +0100

    Recieving+printing full chatroom name

--- Server.py
+++ Server.py
@@ -3,10 +3,11 @@
 #
 import struct
 import binascii
+import socket
 from socket import *
 
 serverPort = 12000
-IPaddress = '123.123.12'
+IPaddress = '123.123.12.1'
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
@@ -23,13 +24,20 @@ while 1:
         sys.exit("Received disconnect message.  Shutting down.")
     elif recieved:
         data = str(recieved)
-        data = data.split()
-        if data[0] == "JOIN_CHATROOM:":
-            chatroomName = data[1]
+        data2 = data.split()
+        if data2[0] == "JOIN_CHATROOM:":
+            data3 = data.splitlines()
+            data4 = data3[0]
+            data4 = data4.split()
+            nameLength = len(data4) - 1
             clientIP = data[3]
             port = data[5]
             clientName = data[7]
-            print 'JOINED_CHATROOM: ', chatroomName, '\n', 'SERVER_IP: ', IPaddress,'\n', 'PORT', serverPort,'\n', 'ROOM_REF','\n',  'JOIN_ID', joinID, '\n'
+            print 'JOINED_CHATROOM: '
+            for i in range(1, nameLength+1):
+                print data4[i],
+            
+            print '\n', 'SERVER_IP: ', IPaddress,'\n', 'PORT', serverPort,'\n', 'ROOM_REF','\n',  'JOIN_ID', joinID, '\n'
             joinID = joinID + 1
 
 
